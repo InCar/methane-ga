5984295805e7d653d33fa096d3d7ca975c70a69b
孙俊 <strange001@163.com>
2020-01-13 14:19:18 +0800

    Dev (#1)
    * 初始化
    
    * 初始化
    
    * travis中增加gradle安装脚本
    
    * 增加springBoot启动入口

---
+++ src/main/java/com/incarcloud/App.java
@@ -0,0 +1,23 @@
+package com.incarcloud;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @desc
+ * @date 2020/1/13
+ */
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
